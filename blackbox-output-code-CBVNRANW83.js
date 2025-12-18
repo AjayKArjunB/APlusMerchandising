44b19f5125d57ef399181b810ed2650fc4979c4c
AjayKArjunB <arjunsbisht21@gmail.com>
2025-12-18 22:34:08 +0530

    add HTML , .css, Java and the PPT for description

---
+++ blackbox-output-code-CBVNRANW83.js
@@ -0,0 +1,5 @@
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+
+// Add more JS for cart functionality, form submission, etc., if expanding to e-commerce.
